c7f81f47b6625ae971d941928d0def3677e1ebaa
Bento <david.bento-pereira@lilo.org>
2022-02-24 15:42:34 +0100

    Add User controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "welcome/index"
-  root to: "welcome#index"
+  get 'welcome/index'
+  root to: 'welcome#index'
 
+  resources :users
   devise_for :users
 
   devise_scope :user do
