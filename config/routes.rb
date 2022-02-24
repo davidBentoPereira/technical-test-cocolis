683dcfdb06deaa045b0f9262a0e8b88de4851e55
Bento <david.bento-pereira@lilo.org>
2022-02-24 10:07:23 +0100

    Add devise views + login, signup and logout links in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "welcome/index"
   root to: "welcome#index"
+
+  devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
