72067560a7bcfe87b46c48ec929ca02893282bc1
Bento <david.bento-pereira@lilo.org>
2022-02-25 16:18:26 +0100

    Manage permissions for User & Devise::Invitations resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
+
+  # Overide Devise invitation to manage permissions
+  def authenticate_inviter!
+    send(:"authenticate_#{resource_name}!", force: true).tap do |inviter|
+      @current_ability = ::Ability.new(inviter)
+      authorize! :invite, User
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
