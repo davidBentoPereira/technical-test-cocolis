836faf33cda65c0872481fcaaad9789bbc8d23ac
Bento <david.bento-pereira@lilo.org>
2022-02-25 14:41:59 +0100

    Add "Destroy account" for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:invite, keys: [:admin])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[password password_confirmation current_password])
   end
 end
