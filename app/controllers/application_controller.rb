2e68705622b788dd3e42cebaf23d756bf46aa253
Bento <david.bento-pereira@lilo.org>
2022-02-25 10:32:58 +0100

    Add choose role when inviting user + fill name when accepting invitation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:invite, keys: [:admin])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[first_name last_name])
+  end
 end
