5989008a697f85511ff917bfdd48fc2b020c9e46
Bento <david.bento-pereira@lilo.org>
2022-02-26 21:52:45 +0100

    Edit rspec config with devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,12 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   # Require FactoryBot
   require 'support/factory_bot'
+
+  RSpec.configure do |config|
+    # For Devise > 4.1.1
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include Devise::Test::IntegrationHelpers, type: :request
+    # Use the following instead if you are on Devise <= 4.1.1
+    # config.include Devise::TestHelpers, :type => :controller
+  end
 end
